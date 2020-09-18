c1f28eea0d7e49406b1cbd1c0bf48b91521bb7d9
okgyuri1109 <okgyuri1109@naver.com>
2020-09-18 16:25:33 +0900

    READ ME 수정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
 
+function Food({fav}) {
+    return (
+        <h3>I love {fav}</h3>
+    );
+}
 function App() {
- // return <div className="App" />;
- return <div>Hello^________^!!!!!!!!!!</div>;
+return (
+ 
+ <div>
+    <h1>Hello</h1>
+    <Food fav="햄버거"/>
+    <Food fav="콜라"/>
+    <Food fav="치즈볼"/>
+    <Food fav="칼국수"/>
+</div>
+ );
 }
 
 export default App;

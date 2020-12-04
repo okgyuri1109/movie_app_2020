e1bd6a3920239e642fb6293930a93eb94335630a
okgyuri1109 <okgyuri1109@naver.com>
2020-12-05 04:09:15 +0900

    edit

---
+++ src/routes/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './About.css';
+
+function About() {
+    return (
+        <div className="about__container">
+            <span>
+                "배규리의 무비앱."
+            </span>
+            <span>- BaeGyuri, 2020</span>
+        </div>
+    );
+}
+
+export default About; 
